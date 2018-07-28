e98600c1500514623d804d46dd0f057bcf72da3a
SolomonNdungu <ssouljabee@gmail.com>
2018-07-28 14:14:27 +0300

    Add files via upload
    These are the files in My Codes

---
+++ index.js
@@ -0,0 +1,11 @@
+function openNav() {
+	document.getElementById("mysidenav").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+	document.body.style.backgroundColor = "rgb(232,231,230)";
+}
+
+function closeNav() {
+	document.getElementById("mysidenav").style.width = "20px";
+	document.getElementById("main").style.marginLeft = "20px";
+	document.body.style.backgroundColor = "white";
+}
